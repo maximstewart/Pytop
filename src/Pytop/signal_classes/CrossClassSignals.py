56627de3334b189f200f0ab7309deeb69a0cda1e
Maxim Stewart <maximstewart1@gmail.com>
2020-03-29 23:25:58 -0500

    added a clock

--- src/Pytop/signal_classes/CrossClassSignals.py
+++ src/Pytop/signal_classes/CrossClassSignals.py
@@ -1,6 +1,8 @@
-# Gtk imports
-
 # Python imports
+from datetime import datetime
+
+# Gtk imports
+from gi.repository import GObject
 
 # Application imports
 
@@ -9,6 +11,23 @@ class CrossClassSignals:
     def __init__(self, settings):
         self.settings     = settings
         self.builder      = self.settings.returnBuilder()
+        self.timeLabel    = self.builder.get_object("timeLabel")
+        self.displayclock()
+        self.startClock()
+
+
+    # Displays Timer
+    def displayclock(self):
+        now     = datetime.now()
+
+        # timeStr = now.strftime("%H:%M %m/%d/%y")
+        timeStr = now.strftime("%I:%M %p  %m/%d/%y")
+        self.timeLabel.set_label(timeStr)
+        return True
+
+    # Starting or clock
+    def startClock(self):
+        GObject.timeout_add(59000, self.displayclock)
 
 
     def closePopup(self, widget, data=None):
